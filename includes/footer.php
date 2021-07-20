cd7b8433e6c8e5ad92af8bd877e3de9325a50d59
bnikunj <85343562+bnikunj@users.noreply.github.com>
2021-07-20 16:09:09 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Shopify | All Rights Reserved | Contact Us: +91 97122 60714</b></span></div>
+    <div class="social-links container text-center">
+        <a href="https://www.twitter.com/BhuvaNikunj6/"><span class="text-muted"><i class="fa fa-twitter"></i></span></a>
+        <a href="https://www.facebook.com/nikunj.bhuva.965/"><span class="text-muted"><i class="fa fa-facebook"></i></span></a>
+        <a href="https://www.instagram.com/_nik.bhuva/"><span class="text-muted"><i class="fa fa-instagram"></i></span></a>
+        <a href="https://www.linkedin.com/in/nikunj-bhuva-0ba2831a2/"><span class="text-muted"><i class="fa fa-linkedin"></i></span></a>
+    </div>
+</footer>
+    
